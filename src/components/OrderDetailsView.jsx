fdbce4473765cabdc867660402226d4e33b95aa1
prasanthoyasco <prasanthoyasco@gmai.com>
2025-07-25 13:59:22 +0530

    Fix: corrected import path case for orderListApi

--- src/components/OrderDetailsView.jsx
+++ src/components/OrderDetailsView.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getOrderById } from "../API/orderListApi";
+import { getOrderById } from "../api/orderListApi";
 import { MapPin, Mail, Phone, MessageCircleMore,Loader,
   ChefHat,
   Truck,
