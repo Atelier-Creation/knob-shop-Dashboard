013948985f3128789b6468f8e8b434f73ede5da7
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2025-08-23 16:44:37 +0530

    updated search box functionality

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,7 +23,7 @@ const navItems = [
     icon: <Package size={20} />,
     children: [
       // { label: "Add Products", path: "/categories-products/add" },
-      { label: "Dashboard", path: "/categories-products/category" },
+      { label: "Add category", path: "/categories-products/category" },
       { label: "Product List", path: "/categories-products/product-list" },
       // { label: "Bulk Upload", path: "/categories-products/bulk-add-product" },
       { label: "Product Status", path: "/categories-products/product-status" },
